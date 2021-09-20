b71c3ce0dc76fcd4586b79b7a30cbb955096434b
danielnoo <dannynoo@gmail.com>
2021-09-20 17:20:37 -0400

    completed displayResults function as well as event listener function

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const artApp = {};
 artApp.apiKey = 'K8gsyEQB';
 
 // make api call to get artist
-artApp.getPieces = function () {
+artApp.getPieces = function (query) {
   const url = new URL('https://www.rijksmuseum.nl/api/en/collection');
   url.search = new URLSearchParams({
     key: artApp.apiKey,
     imgonly: true,
-    q: 'monkeys'
+    q: query
 
   });
   fetch(url)
     .then((results) => {
       return results.json();
     }).then((jsonData) => {
+      document.querySelector('#artwork').innerHTML = '';
       artApp.displayPieces(jsonData.artObjects);
     });
 };
@@ -22,15 +23,42 @@ artApp.getPieces = function () {
 // display arts on the page
 
 artApp.displayPieces = function(artPieces) {
-  console.log(artPieces)
+  
+  artPieces.forEach(artObject => {
+    const title = document.createElement('h2');
+    title.innerText = artObject.title;
+    
+    const artist = document.createElement('p');
+    artist.innerText = artObject.principalOrFirstMaker;
+    
+    const image = document.createElement('img');
+    image.src = artObject.webImage.url;
+    image.alt = artObject.title;
+
+    const artPiece = document.createElement('div');
+    artPiece.classList.add('piece');
+    artPiece.append(title, artist, image);
+
+    document.querySelector('#artwork').append(artPiece);
+  })
 };
 
 
+
+artApp.setUpEventListener = function() {
+  document.querySelector('#animal').addEventListener('change', function() {
+    artApp.getPieces(this.value);
+  })
+}
 
 
 artApp.init = function () {
-  console.log('lets get arting')
-  artApp.getPieces();
+  
+  
+  artApp.setUpEventListener();
 };
+
+
+
 
 artApp.init();
